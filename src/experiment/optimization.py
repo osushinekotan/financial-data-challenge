b111cacf7ad38f2c4248d5f36079521c4826190f
Masato Toba <m.toba@astamuse.co.jp>
2024-02-10 14:32:24 +0900

    add : optimize f1 macro v2

--- src/experiment/optimization.py
+++ src/experiment/optimization.py
@@ -33,3 +33,25 @@ def opt_macro_f1_score(
     macro_f1 = lambda y_true, y_pred: f1_score(y_true, y_pred, average="macro")  # noqa: E731
     th = optimize_threshold(y_true, y_pred, metrics=macro_f1)
     return {"th": th, "score": macro_f1(y_true, y_pred >= th)}
+
+
+def decode_v2(
+    y_pred: np.ndarray,
+    num_positive: int,
+) -> np.ndarray:
+    pred_label_idx = np.argsort(y_pred)[::-1][:num_positive]
+    y_pred_label = np.zeros_like(y_pred)
+    y_pred_label[pred_label_idx] = 1
+    return y_pred_label
+
+
+def opt_macro_f1_score_v2(
+    y_true: np.ndarray,
+    y_pred: np.ndarray,
+) -> dict[str, float]:
+    n = len(y_pred)
+    num_positive = np.sum(y_true)
+    y_pred_label = decode_v2(y_pred, num_positive)
+    score = f1_score(y_true, y_pred_label, average="macro")
+
+    return {"num_all": n, "num_positive": num_positive, "pos_ratio": num_positive / n, "score": score}
